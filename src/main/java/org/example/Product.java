0478c8d7e2e79fdac6e1ed639c4c31ae2b377a55
Farid Karimi <fkarimi8320@gmail.com>
2023-03-28 15:52:05 +0430

    classes has been created

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Product {
+    /*
+    book
+    clothes
+    electronic
+     */
+}
---
+++ src/main/java/org/example/Shop.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Shop {
+    private String name;
+    private String webAddress;
+    private String phoneNumber;
+    private ArrayList<Account> accountList;
+    private ArrayList<Product> productList;
+    private ArrayList<Order> orderList;
+    private double totalProfit;
+
+    public Shop(String name, String webAddress, String phoneNumber, double totalProfit) {
+        this.name = name;
+        this.webAddress = webAddress;
+        this.phoneNumber = phoneNumber;
+        this.accountList = new ArrayList<>();
+        this.productList = new ArrayList<>();
+        this.orderList = new ArrayList<>();
+        this.totalProfit = totalProfit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebAddress() {
+        return webAddress;
+    }
+
+    public void setWebAddress(String webAddress) {
+        this.webAddress = webAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ArrayList<Product> productList) {
+        this.productList = productList;
+    }
+
+    public ArrayList<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    public double getTotalProfit() {
+        return totalProfit;
+    }
+
+    public void setTotalProfit(double totalProfit) {
+        this.totalProfit = totalProfit;
+    }
+}
